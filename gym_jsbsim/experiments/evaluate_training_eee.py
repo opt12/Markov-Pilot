773e02cb53f61ce40acc40f2d8f838f5d5180e66
Felix <github@embedded-engineering.de>
2020-05-15 00:09:20 +0200

    - reworked the evaluation to new standard

--- gym_jsbsim/experiments/evaluate_training_eee.py
+++ gym_jsbsim/experiments/evaluate_training_eee.py
@@ -11,103 +11,6 @@ import gym_jsbsim.environment.properties as prp
 best_score_n = []  #we don't like globals, but it really helps here
 eval_number = 0
 
-'''
-The old style before the invention of the agent container
-def evaluate_training(agents, env, lab_journal = None, add_exploration_noise=False, store_evaluation_experience = True):
-    global best_score_n 
-    if len(best_score_n) != env.n:
-        best_score_n = np.zeros(env.n)
-
-    tgt_flight_path_deg = -6.5
-    tgt_roll_angle_deg  = 10
-    initial_path_angle_gamma_deg = 0
-    initial_roll_angle_phi_deg   = 0
-    initial_fwd_speed_KAS        = 75
-    initial_aoa_deg              = 1.0
-
-    env.change_setpoints({ prp.flight_path_deg: tgt_flight_path_deg
-                         , prp.roll_deg:  tgt_roll_angle_deg
-                        })
-    env.set_initial_conditions( { prp.initial_u_fps: 1.6878099110965*initial_fwd_speed_KAS
-                                , prp.initial_flight_path_deg: initial_path_angle_gamma_deg
-                                , prp.initial_roll_deg: initial_roll_angle_phi_deg
-                                , prp.initial_aoa_deg: initial_aoa_deg
-                                })
-
-    exploration_noise = add_exploration_noise   #to have a handle on that in the debugger
-    obs_n = env.reset()
-    [ag.reset_notifier() for ag in agents]
-
-    env.showNextPlot(True, True)
-    terminal = False
-    score_n = np.zeros(env.n)
-    steps = 0
-    while not terminal:
-        # get action
-        action_n = [agent.action(obs, exploration_noise) for agent, obs in zip(agents,obs_n)]
-
-        new_obs_n, reward_n, done_n, info_n = env.step(action_n)
-
-        terminal = env.is_terminal()   #there may be agent independent terminal conditions like the number of episode steps
-
-        if store_evaluation_experience:
-            # also store the evaluation experience into the replay buffer. this is valid experience, so use it
-            [agent.process_experience(obs_n[i], action_n[i], reward_n[i], new_obs_n[i], done_n[i], terminal) for i, agent in enumerate(agents)]
-
-        obs_n = new_obs_n
-
-        score_n += reward_n     # the action includes noise!!!
-        steps += 1
-        if steps == int(0.5 *60 / env.dt):
-            tgt_flight_path_deg = -6.5
-            tgt_roll_angle_deg  = -10
-            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg })
-        if steps == int(1 *60 / env.dt):
-            tgt_flight_path_deg = -7.5
-            tgt_roll_angle_deg  = -10
-            env.change_setpoints({ prp.flight_path_deg: tgt_flight_path_deg })
-        if steps == int(1.5 *60 / env.dt):
-            tgt_flight_path_deg = -7.5
-            tgt_roll_angle_deg  = 10
-            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg })
-
-        terminal = any(done_n) or terminal
-        #env.render()
-    print("\tTest yielded a score of : [", end="")
-    print(*["%.2f"%sc for sc in score_n], sep = ", ", end="")
-    print("].")
-
-    if lab_journal:
-        for i, ag in enumerate(agents):
-            eval_dict = {
-                'entry_type': ag.name,
-                'reward': '{:.2f}'.format(score_n[i]),
-                'steps': ag.train_steps,
-            }
-
-            #save the agents' state
-            if ag.agent_save_path and hasattr(ag, 'agent'):
-                filename = os.path.join(ag.agent_save_path, ag.name, f'{ag.name}_rwd-{score_n[i]:06.2f}_steps-{ag.train_steps}')
-                ag.agent.save_models(filename)
-                eval_dict.update({'path': 'file://'+filename})
-                if best_score_n[i] < score_n[i]:
-                    print("%s: Best score updated: %.3f -> %.3f" % (ag.name, best_score_n[i], score_n[i]))
-                    bestname = os.path.join(ag.agent_save_path, ag.name, f'{ag.name}_best')
-                    copyfile(filename, bestname)
-                    best_score_n[i] = score_n[i]
-
-            lab_journal.append_evaluation_data(eval_dict)
-
-    # name = env.meta_dict['model_base_name']
-    # discriminator = env.meta_dict['model_discriminator']
-    # env.set_meta_information(model_discriminator = discriminator)
-    # agent.save_models(name_discriminator=discriminator)    
-    # if best_reward is None or best_reward < score:
-    #     if best_reward is not None:
-    #         print("Best reward updated: %.3f -> %.3f" % (best_reward, score))
-    #     agent.save_models(name_discriminator= name + '_best')
-    #     best_reward = score
-'''
 
 def evaluate_training(agent_container, env, lab_journal = None, add_exploration_noise=False, store_evaluation_experience = True):
     global best_score_n 
@@ -115,22 +18,25 @@ def evaluate_training(agent_container, env, lab_journal = None, add_exploration_
         best_score_n = np.zeros(agent_container.m)
 
     tgt_flight_path_deg = -6.5
-    tgt_roll_angle_deg  = 10
-    target_kias = 92
+    tgt_roll_angle_deg  = 15
+    tgt_sideslip_deg    = 0
+    # target_kias = 92  #not used currently as only gliding descent is regulated
     initial_path_angle_gamma_deg = 0
     initial_roll_angle_phi_deg   = 0
-    initial_fwd_speed_KAS        = 75
-    initial_aoa_deg              = 1.0
+    initial_fwd_speed_KAS        = env.aircraft.get_cruise_speed_fps()*0.9 / 1.6878099110965
+    initial_aoa_deg              = 1.0  #arbitrary value but not far from a stable flight attitude
 
-    env.change_setpoints({ prp.flight_path_deg: tgt_flight_path_deg
-                         , prp.roll_deg:  tgt_roll_angle_deg
-                         , prp.indicated_airspeed: target_kias
+    env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                            prp.flight_path_deg: tgt_flight_path_deg,
+                            prp.sideslip_deg: tgt_sideslip_deg
                         })
     env.set_initial_conditions( { prp.initial_u_fps: 1.6878099110965*initial_fwd_speed_KAS
                                 , prp.initial_flight_path_deg: initial_path_angle_gamma_deg
                                 , prp.initial_roll_deg: initial_roll_angle_phi_deg
                                 , prp.initial_aoa_deg: initial_aoa_deg
                                 })
+
+    env.change_next_episode_length(4*60)    #change the test-episode length to 4 minutes.
 
     exploration_noise = add_exploration_noise   #to have a handle on that in the debugger
     obs_n = env.reset()
@@ -157,27 +63,65 @@ def evaluate_training(agent_container, env, lab_journal = None, add_exploration_
 
         score_m += [exp.rew for exp in agent_experience_m]
         steps += 1
-        if steps == int(0.5 *60 / env.dt):
-            tgt_flight_path_deg = -6.5
-            tgt_roll_angle_deg  = -10
-            target_kias = 72
+        if steps == int(0.5 * 60 / env.dt): #after half a minute
+            tgt_flight_path_deg = -6
+            tgt_roll_angle_deg  = 0
+            # target_kias = 72
 
-            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg 
-                                , prp.indicated_airspeed: target_kias})
-        if steps == int(1 *60 / env.dt):
+            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                                   prp.flight_path_deg: tgt_flight_path_deg,
+                                   prp.sideslip_deg: tgt_sideslip_deg
+                                })
+
+        if steps == int(1 *60 / env.dt):    #after a minute
             tgt_flight_path_deg = -7.5
-            tgt_roll_angle_deg  = -10
-            target_kias = 100
+            tgt_roll_angle_deg  = 0
+            # target_kias = 100
 
-            env.change_setpoints({ prp.flight_path_deg: tgt_flight_path_deg  
-                                , prp.indicated_airspeed: target_kias})
-        if steps == int(1.5 *60 / env.dt):
+            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                                   prp.flight_path_deg: tgt_flight_path_deg,
+                                   prp.sideslip_deg: tgt_sideslip_deg
+                                })
+
+        if steps == int(1.5 *60 / env.dt): #after one and a half minutes
             tgt_flight_path_deg = -7.5
-            tgt_roll_angle_deg  = 10
-            target_kias = 80
+            tgt_roll_angle_deg  = -20
+            # target_kias = 80
 
-            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg  
-                                , prp.indicated_airspeed: target_kias})
+            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                                   prp.flight_path_deg: tgt_flight_path_deg,
+                                   prp.sideslip_deg: tgt_sideslip_deg
+                                })
+
+        if steps == int(2.0 *60 / env.dt): #after two minutes
+            tgt_flight_path_deg = -7.5
+            tgt_roll_angle_deg  = 15
+            # target_kias = 80
+
+            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                                   prp.flight_path_deg: tgt_flight_path_deg,
+                                   prp.sideslip_deg: tgt_sideslip_deg
+                                })
+
+        if steps == int(2.5 *60 / env.dt): #after two and a half minutes
+            tgt_flight_path_deg = -7.5
+            tgt_roll_angle_deg  = 0
+            # target_kias = 80
+
+            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                                   prp.flight_path_deg: tgt_flight_path_deg,
+                                   prp.sideslip_deg: tgt_sideslip_deg
+                                })
+
+        if steps == int(3 *60 / env.dt): #after three minutes
+            tgt_flight_path_deg = -6
+            tgt_roll_angle_deg  = 0
+            # target_kias = 80
+
+            env.change_setpoints({ prp.roll_deg:  tgt_roll_angle_deg,
+                                   prp.flight_path_deg: tgt_flight_path_deg,
+                                   prp.sideslip_deg: tgt_sideslip_deg
+                                })
 
         terminal = any(done_n) or terminal
         #env.render()
